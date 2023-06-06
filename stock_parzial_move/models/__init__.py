f45470b00e5d9aa346d19254c4b9214ace597f8a
zeroincombenze <info@shs-av.com>
2023-06-06 17:19:08 +0200

    [IMP] Clean repo

--- stock_parzial_move/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright 2017-2018, Didotech srl (http://www.didotech.com).
-# Copyright 2017-2018, Andrei Levin <andrei.levin@didotech.com>
-# Copyright 2018, Associazione Odoo Italia <https://odoo-italia.org>
-#
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-#
-from . import stock
